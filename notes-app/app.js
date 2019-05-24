e1e601b633eaa62aafe034d0eda3fe4e25e4232e
lukinsb <lucas.baiao@hotmail.com>
2019-05-24 16:40:14 -0300

    teste cor

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,12 @@
 const validator = require('validator')
 const getNotes = require('./notes.js')
-
+const chalk = require('chalk')
 
 console.log(getNotes())
 
 console.log(validator.isURL('uolbr'))
+console.log(validator.isURL('uol.com.br'))
+
+
+console.log(chalk.bgGreen('Success'))
+
