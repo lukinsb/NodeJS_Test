f11e7b380aa992dbd528fcee82d79eb05d623fb1
lukinsb <lucas.baiao@hotmail.com>
2019-05-27 16:14:49 -0300

    teste4

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,5 @@
 const getNotes = require('./notes.js')
-const chalk = require('chalk')
+const chalk = require('chalk')       
 const yargs = require('yargs')
 
 yargs.version('1.0.0')
@@ -8,8 +8,51 @@ yargs.command({
 
     command: 'add',
     describe: 'Add a new note',
-    handler: function add() {
-        console.log('Adding new note!')
-        
+    builder: {
+        title:{
+            describe:'Note Title',
+            demandOption: true,
+            type: 'string'
+        },
+        body:{
+            describe:'Note Body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title: '+argv.title)
+        console.log('Body: '+argv.body)
     }
 })
+
+yargs.command({
+
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing a note!')
+    }
+})
+
+yargs.command({
+
+    command: 'list',
+    describe: 'List the notes',
+    handler: function () {
+        console.log('Listing the notes!')
+    }
+})
+
+yargs.command({
+
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note!')
+    }
+})
+
+
+yargs.parse()
+//console.log(yargs.argv)
