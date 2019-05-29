2c1ce51fb6af8857f0605faca1235324b4008a25
lukinsb <lucas.baiao@hotmail.com>
2019-05-29 17:32:55 -0300

    ok

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,9 +2,9 @@ const request = require('request')
 
 const url = 'https://api.darksky.net/forecast/7fff670547681c9e8767ebc912b995de/-23.5702158,-46.6461033?lang=pt&units=si'
 
-request({
-    url: url,
-    json: true
-}, (error, response) => {
-    console.log(response.body.currently)
+request({url: url, json: true}, (error, response) => {
+
+    var probab = response.body.currently.precipProbability * 100
+    console.log('It is currently ' + response.body.currently.temperature + ' degrees out and there is a ' + probab +'% chance of rain')
+
 })
