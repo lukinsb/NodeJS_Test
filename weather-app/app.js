b3dd8634a1ee8530d02123993ada20c7ad05f00d
lukinsb <lucas.baiao@hotmail.com>
2019-05-29 17:10:59 -0300

    ok

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,10 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/7fff670547681c9e8767ebc912b995de/-23.5702158,-46.6461033'
+const url = 'https://api.darksky.net/forecast/7fff670547681c9e8767ebc912b995de/-23.5702158,-46.6461033?lang=pt&units=si'
 
 request({
-    url: url
+    url: url,
+    json: true
 }, (error, response) => {
-
-    const data = JSON.parse(response.body)
-    console.log(data)
+    console.log(response.body.currently)
 })
